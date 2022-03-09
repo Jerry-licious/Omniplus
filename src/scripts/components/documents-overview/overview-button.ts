f78db98ef56e894c74443fda93cab9e8da2a4622
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2022-03-09 11:38:15 -0500

    Potentially fixed a bug where the lack of a document summary page on the teacher version of Omnivox halts the script.

--- src/scripts/components/documents-overview/overview-button.ts
+++ src/scripts/components/documents-overview/overview-button.ts
@@ -6,32 +6,36 @@ export function injectDocumentsOverviewButtonToLea() {
     const summaryAnchorContainer = Array.from(document.querySelectorAll('.SousMenuItemMesClasses'))
         // There are 19 of them without special identifiable features apart from their text content.
         .find((element) => element.textContent == 'Summary of documents');
-    // That element is only not present when the user is already on the overview page, so redirecting back to
-    // the current page will do the job.
-    const overviewLink = summaryAnchorContainer ?
-        (<HTMLAnchorElement>summaryAnchorContainer.firstElementChild).href : window.location.href;
 
-    // All the buttons are contained in this element.
-    const buttonsContainer = document.querySelector('#region-raccourcis-services-skytech');
-    buttonsContainer.insertBefore(
-        new ElementBuilder('a')
-            // Mirror the style of the Lea button.
-            .withStyleClasses('raccourci', 'id-service_CVIP', 'code-groupe_lea', 'documents-overview-icon-parent')
-            .withAttribute('href', overviewLink)
-            .withAttribute('title', 'Documents Overview')
-            .withChildren(
-                new ElementBuilder('div')
-                    // Mirror the style of the Lea button, but with the material icons class added.
-                    .withStyleClasses('svg-icon', 'material-icons')
-                    // Documents icon name
-                    .withText('description')
-                    .build(),
-                new ElementBuilder('div')
-                    .withStyleClasses('titre')
-                    .withText('Docs')
-                    .build()
-            )
-            .build(),
-        // The last child is the MIO button, put the overview before the MIO button so it's together with Lea.
-        buttonsContainer.lastElementChild);
+    // Potential fix for the teacher version of the website where the summary may not be present.
+    if (summaryAnchorContainer) {
+        // That element is only not present when the user is already on the overview page, so redirecting back to
+        // the current page will do the job.
+        const overviewLink = summaryAnchorContainer ?
+            (<HTMLAnchorElement>summaryAnchorContainer.firstElementChild).href : window.location.href;
+
+        // All the buttons are contained in this element.
+        const buttonsContainer = document.querySelector('#region-raccourcis-services-skytech');
+        buttonsContainer.insertBefore(
+            new ElementBuilder('a')
+                // Mirror the style of the Lea button.
+                .withStyleClasses('raccourci', 'id-service_CVIP', 'code-groupe_lea', 'documents-overview-icon-parent')
+                .withAttribute('href', overviewLink)
+                .withAttribute('title', 'Documents Overview')
+                .withChildren(
+                    new ElementBuilder('div')
+                        // Mirror the style of the Lea button, but with the material icons class added.
+                        .withStyleClasses('svg-icon', 'material-icons')
+                        // Documents icon name
+                        .withText('description')
+                        .build(),
+                    new ElementBuilder('div')
+                        .withStyleClasses('titre')
+                        .withText('Docs')
+                        .build()
+                )
+                .build(),
+            // The last child is the MIO button, put the overview before the MIO button so it's together with Lea.
+            buttonsContainer.lastElementChild);
+    }
 }
